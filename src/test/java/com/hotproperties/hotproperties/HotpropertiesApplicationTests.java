88adde0aaca3f0ee455d0b030bd99d0a5bcf766c
navin <navinch108@gmail.com>
2025-05-31 10:34:00 -0500

    initial commit

---
+++ src/test/java/com/hotproperties/hotproperties/HotpropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotproperties.hotproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotpropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
