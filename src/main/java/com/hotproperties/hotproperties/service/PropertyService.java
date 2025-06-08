5e4f8ceb5228a38e3b12fa300e1e5ef52af841a2
Waqas <warain@depaul.edu>
2025-06-07 20:15:28 -0400

    more features

---
+++ src/main/java/com/hotproperties/hotproperties/service/PropertyService.java
@@ -0,0 +1,10 @@
+package com.hotproperties.hotproperties.service;
+
+import com.hotproperties.hotproperties.entity.Property;
+
+import java.util.List;
+
+public interface PropertyService {
+
+    List<Property> findAllProperties();
+}
