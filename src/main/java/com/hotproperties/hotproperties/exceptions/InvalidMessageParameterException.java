660113540ed2518830d470b26c2ac8f54554fd65
WSArain <157272225+WSArain@users.noreply.github.com>
2025-06-08 23:04:02 -0400

    Revert "latest"

--- src/main/java/com/hotproperties/hotproperties/exceptions/InvalidMessageParameterException.java
+++
@@ -1,7 +0,0 @@
-package com.hotproperties.hotproperties.exceptions;
-
-public class InvalidMessageParameterException extends RuntimeException {
-    public InvalidMessageParameterException(String message) {
-        super(message);
-    }
-}
